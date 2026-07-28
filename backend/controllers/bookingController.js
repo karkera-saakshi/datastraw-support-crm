dead7e6943c8795b2cd9fe5548ee58c9f6035f91
karkera-saakshi <saakshikarkera81@gmail.com>
2026-07-28 18:59:16 +0530

    Controller Setup

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -1,19 +1,19 @@
 const bookingModel = require("../models/bookingModel");
 
 let createTicket = (req, res) =>{
-
+    bookingModel.createTicket(req.body, res);
 }
 
 let getAllTickets = (req, res) => {
-
+    bookingModel.getAllTickets(req.query, res);
 }
 
 let getTicketById = (req, res) =>{
-    
+    bookingModel.getTicketById(req.params.ticket_id, res);
 }
 
 let updateTicket = (req, res) =>{
-
+    bookingModel.updateTicket(req.params.ticket_id, req.body, res);
 }
 
 module.exports = { createTicket, getAllTickets, getTicketById, updateTicket};
