22f2d08c117ec8eed986d8121dc4f4be74b253b4
karkera-saakshi <saakshikarkera81@gmail.com>
2026-07-29 06:29:47 +0530

    feat: implement ticket creation endpoint and database logic

--- backend/models/bookingModel.js
+++ backend/models/bookingModel.js
@@ -1,6 +1,6 @@
 const { MongoClient, ObjectId } = require("mongodb");
 
-let url = process.env.MONGO_URL;
+let url = process.env.MONGO_URI;
 let getCollection = () => {
     let client = new MongoClient(url);
     client.connect();
@@ -9,9 +9,16 @@ let getCollection = () => {
     return { client, coll};
 };
 
-let createTicket  = () =>
+let createTicket  = async (obj, res) =>
 {
-
+    let client = new MongoClient(url);
+    await client.connect();
+    let db = client.db("support-CRM");
+    let coll = db.collection("tickets");
+    await coll.insertOne(obj)
+    .then((result)=> res.send(result))
+    .catch((err)=>res.status(500).send(err))
+    .finally (()=>client.close())
 }
 
 let getAllTickets = () =>
@@ -24,7 +31,7 @@ let getTicketById = () =>
 
 }
 
-let updateTicket = () =>
+let updateTicket = (id, data, res) =>
 {
     
 }
