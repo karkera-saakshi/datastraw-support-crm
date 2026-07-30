69a7987e9f1e91160389098b45563e4da1227df2
karkera-saakshi <saakshikarkera81@gmail.com>
2026-07-30 07:16:34 +0530

    Resolved Deployment Error

--- backend/server.js
+++ backend/server.js
@@ -7,3 +7,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/tickets",bookingRoutes);
 const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+    console.log(`Server is running and listening on port ${PORT}`);
+});
